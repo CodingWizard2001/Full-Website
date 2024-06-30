d833b35ea31831aaee86e5134dbfa31e275a636e
CodingWizard2001 <goswami.rahul1002@gmail.com>
2024-07-01 00:24:13 +0530

    Completed on 1-07-2024

--- Delivery/script.js
+++ Delivery/script.js
@@ -17,3 +17,24 @@ window.onscroll = () =>{
     menu.classList.remove('bx-x');
     navBar.classList.remove('active');
 }
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 1500,
+    delay: 200,
+    reset: true,
+})
+sr.reveal('.left-section',{origin:'left'})
+sr.reveal('.right-section',{origin:'right'})
+sr.reveal('.first img',{origin:'right'})
+sr.reveal('.first h5',{origin:'left'})
+sr.reveal('.first p',{origin:'left'})
+sr.reveal('.img-sec',{origin:'bottom'})
+sr.reveal('.context-sec',{origin:'top'})
+sr.reveal('.box',{origin:'left',interval: 200})
+sr.reveal('.left-part',{origin:'top'})
+sr.reveal('.right-part',{origin:'bottom'})
+sr.reveal('.btn-sec',{origin:'right'})
+sr.reveal('.form-sec',{delay:600})
+sr.reveal('footer',{delay:600,origin: 'left'})
