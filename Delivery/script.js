7e3546c1a061a7400ff8ce337e8b8cbdce02270d
CodingWizard2001 <goswami.rahul1002@gmail.com>
2024-06-30 20:55:16 +0530

    Contact and Footer Section are added with hamburger menu

--- Delivery/script.js
+++ Delivery/script.js
@@ -3,3 +3,17 @@ let header = document.querySelector('header');
 window.addEventListener('scroll',()=>{
     header.classList.toggle('shadow',window.scrollY > 0);
 });
+
+// Hamburger Menu
+let menu = document.querySelector('#menu-icon');
+let navBar = document.querySelector('.navBar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navBar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('bx-x');
+    navBar.classList.remove('active');
+}
